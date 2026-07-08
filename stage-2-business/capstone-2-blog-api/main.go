8f3fb8633c5b63e689cd05c31f723c863c9942ec
gaoshengkai.maple <gaoshengkai.maple@bytedance.com>
2026-07-08 23:59:37 +0800

    fix: complete capstone blog api server crud

--- stage-2-business/capstone-2-blog-api/main.go
+++ stage-2-business/capstone-2-blog-api/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"just-go/stage-2-business/capstone-2-blog-api/auth"
@@ -11,8 +14,17 @@ import (
 )
 
 func main() {
-	_ = server.NewAPI(store.NewMemoryStore(), cache.NewArticleCache(time.Minute), auth.NewTokenManager([]byte("dev-secret")))
+	addr := os.Getenv("ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
 	fmt.Println("Capstone 2 Blog API")
-	fmt.Println("routes: POST /api/register, POST /api/login, GET/POST /api/articles, POST /api/articles/{id}/comments")
+	fmt.Println("routes: POST /api/register, POST /api/login, GET/POST /api/articles, GET/PUT/DELETE /api/articles/{id}, POST /api/articles/{id}/comments")
 	fmt.Println("observability: GET /livez, GET /readyz, GET /metrics")
+	fmt.Printf("listening on %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, buildHandler()))
+}
+
+func buildHandler() http.Handler {
+	return server.NewAPI(store.NewMemoryStore(), cache.NewArticleCache(time.Minute), auth.NewTokenManager([]byte("dev-secret")))
 }
